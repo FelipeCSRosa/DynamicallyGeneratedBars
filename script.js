45ff98315c66244d34c9cc5f3b115b11152058a6
FelipeCSRosa <ffelipecs.rosa@gmail.com>
2022-08-03 02:48:55 -0300

    CSS Changes

--- script.js
+++ script.js
@@ -1,43 +1,33 @@
-const container = document.querySelector('.limiter-container');
+const container = document.querySelector('main');
 
-let arr = [30, 60, 100, 50, 40, 70, 45];
-
-
-let arrData = [];
+let arrAmounts = [];
+let arrDays = [];
 
 for(let i = 0; i < data.dayliAmounts.length; i++){
-  arrData.push(data.dayliAmounts[i].amount)
+  arrAmounts.push(data.dayliAmounts[i].amount)
 }
 
-const max = Math.max(...arrData);
+for(let i = 0; i < data.dayliAmounts.length; i++){
+  arrDays.push(data.dayliAmounts[i].day)
+}
+
+const max = Math.max(...arrAmounts);
 const multplier = 100 / max;
 
 let arrPercents = [];
 
-for(let i = 0; i < arrData.length; i++) {
-  arrPercents.push(arrData[i] * multplier);
+for(let i = 0; i < arrAmounts.length; i++) {
+  arrPercents.push(arrAmounts[i] * multplier);
 }
-
 
 let index = 0;
 
 arrPercents.map(e => {
   container.innerHTML += `
-    <div class="bar" style="height: ${e}%; background-color: ${e == 100 ? '#76b5bc' : '#e3745d'}; color: ${e == 100 ? '#76b5bc' : '#e3745d'}">${arrData[index]}</div>
+    <div class="bar-container" style="height: ${e}%">
+      <div class="bar" style="background-color: ${e == 100 ? '#76b5bc' : '#e3745d'};"></div>
+      <h6>${arrDays[index]}</h6>
+    </div>
   `
   index++;
 })
-
-const bar = document.querySelectorAll('.bar');
-
-for(let i = 0; i < bar.length; i++){
-  bar[i].addEventListener("mouseenter", function( event ) {
-    event.target.classList.add("whiteColor");
-  })
-}
-
-for(let i = 0; i < bar.length; i++){
-  bar[i].addEventListener("mouseout", function( event ) {
-    event.target.classList.remove("whiteColor");
-  })
-}
