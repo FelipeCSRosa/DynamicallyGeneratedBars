c8553c045b5b056fd8b6557f483509082c4804c5
FelipeCSRosa <ffelipecs.rosa@gmail.com>
2022-08-04 19:54:20 -0300

    Some visual changes

--- script.js
+++ script.js
@@ -17,15 +17,17 @@ const multplier = 100 / max;
 let arrPercents = [];
 
 for(let i = 0; i < arrAmounts.length; i++) {
-  arrPercents.push(arrAmounts[i] * multplier);
+  arrPercents.push((arrAmounts[i] * multplier) * 0.8);
 }
+
+console.log(arrPercents)
 
 let index = 0;
 
 arrPercents.map(e => {
   container.innerHTML += `
     <div class="bar-container" style="height: ${e}%">
-      <div class="bar" style="background-color: ${e == 100 ? '#76b5bc' : '#e3745d'};"></div>
+      <div class="bar" style="background-color: ${e == 80 ? '#76b5bc' : '#e3745d'};"></div>
       <h6>${arrDays[index]}</h6>
     </div>
   `
